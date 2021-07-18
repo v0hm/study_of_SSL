a4af0610b7357fae3d21920d1ccf6d3408c7f7e3
VLadimir <v0hmly@gmail.com>
2021-07-18 12:42:03 +0300

    Added config file

---
+++ Config.m
@@ -0,0 +1,18 @@
+status = "restart";
+% restart - возвращение в исходные точки
+% game - сама игра
+% penalty - штрафной удар
+% waiting - ожидание
+
+side = "left";
+% left, right
+
+bots_nums = [5, 4]; %нападающий, вратарь
+bots_start_pos_left = [
+	-500 0;
+	-1250 0
+];
+bots_start_pos_right = [
+	500 0;
+	1250 0
+];
